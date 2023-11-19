7b2752fbb990b5458c8b559ae52e7cea4fbd9df1
Tyuiu.BakhtiyarovDR@outlook.com <dinar_bahtiyarov2005@mail.ru>
2023-11-19 22:23:45 +0500

    Task3

---
+++ Tyuiu.BakhtiyarovDR.Sprint5.Task3.V7.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BakhtiyarovDR.Sprint5.Task3.V7.Lib;
+using System.IO;
+
+namespace Tyuiu.BakhtiyarovDR.Sprint5.Task3.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Users\Dinar\source\repos\Tyuiu.BakhtiyarovDR.Sprint5\Tyuiu.BakhtiyarovDR.Sprint5.Task3.V7\bin\Debug\OutPutFileTask3.bin";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.BakhtiyarovDR.Sprint5.Task3.V7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BakhtiyarovDR.Sprint5.Task3.V7.Lib;
+using System.IO;
+
+namespace Tyuiu.BakhtiyarovDR.Sprint5.Task3.V7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                *");
+            Console.WriteLine("* Тема: Обработка файлов                                                   *");
+            Console.WriteLine("* Задание #3                                                               *");
+            Console.WriteLine("* Вариант #7                                                               *");
+            Console.WriteLine("* Выполнил: Бахтияров Динар Русланович | АСОиУБ-23-3                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Дано выражение f(x) = 1.6*x^3-2.1*x^2+7*x, вычислить его значение при    *");
+            Console.WriteLine("* x = 2, результат сохранить в бинарный файл OutPutFileTask3.bin и вывести *");
+            Console.WriteLine("* на консоль. Округлить до трёх знаков после запятой.                      *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            int x = 2;
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+
+            Console.WriteLine("Результат: ");
+            Console.WriteLine(1.6 * (Math.Pow(x, 3)) - 2.1 * (Math.Pow(x, 2)) + 7 * x);
+
+            Console.ReadKey();
+        }
+    }
+}
